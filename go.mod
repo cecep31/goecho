5c91a5e84f949c138518c997eac39ceea02dc794
cecep31 <cecepjanuardi31@gmail.com>
2021-03-06 16:08:10 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/cecep31/goecho
+
+go 1.16
+
+require github.com/labstack/echo/v4 v4.2.0 // indirect
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/", hello)
+	e.GET("/users/:id", getUser)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+// Handler
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}
+
+// e.GET("/users/:id", getUser)
+func getUser(c echo.Context) error {
+	// User ID from path `users/:id`
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
+}
